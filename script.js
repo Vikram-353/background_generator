3b8fc382eac36c1ffd4caff43dc4b4d21c2cb784
Vikram 353 <vikramranjan71122@gmial.com>
2023-09-21 23:06:09 +0530

    ADD NEW FILES

---
+++ script.js
@@ -0,0 +1,19 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+
+var body = document.getElementById("gradiant");
+function setgradient() {
+    body.style.background = "linear-gradient(to left, " + color1.value + " ,"+ color2.value + ", " + color3.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setgradient);
+color2.addEventListener("input", setgradient);
+color3.addEventListener("input", setgradient);
+
+
+
+
+
